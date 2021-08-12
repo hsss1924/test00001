0457119a9063bd6a8e4a32a24147e92fb6e0b0ed
sunShine <super_hsh@163.com>
2021-08-12 23:32:56 +0800

    up

--- testSwift1/testSwift1/main.swift
+++ testSwift1/testSwift1/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
-
+let somePoint = (1, 1)
+switch somePoint {
+case (0, 0):
+    print("\(somePoint) is at the origin")
+case (_, 0):
+    print("\(somePoint) is on the x-axis")
+case (0, _):
+    print("\(somePoint) is on the y-axis")
+case (-2...2, -2...2):
+    print("\(somePoint) is inside the box")
+default:
+    print("\(somePoint) is outside of the box")
+}
+// 输出“(1, 1) is inside the box”
